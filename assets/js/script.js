9ce37dec91aabd9621d1138d0767bb9acdb53559
Glatigny <f.west@live.fr>
2019-02-05 13:04:48 +0100

    exo 3

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+$(function(){
+    $(document).ready(function(){
+        // je déclare les différentes variables possibles
+        var $gameValue = Math.floor(Math.random() * (100 - 0 + 1)) + 0;         // Nombre à trouver aléatoire entre 0 et 100
+        console.log('Here\'s the answer Mofo! : ' + $gameValue);                // Affichage de la réponse dans la console si on appuie sur F12
+        var $gameTry = 0;                                                       // Nombre d'essai défini à 0 par défaut
+        var $gameResult = $('#gameResult');                                     // Déclaration de la variable contenant l'afficheur des résultats du jeu
+        var nmbVerif = /^[0-9]+$/;                                              // Regex pour ne laisser passer que des chiffres
+        var $gameSearch = $('#gameSearch').val('');                             // Remise à zéro du champ de saisie
+        // Appel de fonction au clic du bouton "Valider"
+        $('#gameValid').on('click', function(){
+            $gameSearch = $('#gameSearch').val();
+            if($gameSearch.match(nmbVerif)){
+                $gameTry++;
+                testNmb();
+            }else{
+                $gameResult.text('Nope! Un nombre entier please !');
+            }
+        });
+        // Appel de fonction au clic de la touche "Entrer" dans le champ de saisie
+        $('#gameSearch').keydown(function(event){
+            if(event.keyCode == 13){
+            $gameSearch = $('#gameSearch').val();
+                if($gameSearch.match(nmbVerif)){
+                    $gameTry++;
+                    testNmb();
+                }else{
+                    $gameResult.text('Nope! Un nombre entier please !');
+                }
+            }
+        });
+        // Cette fonction permet de vérifer si la valeur entrée est égale au résultat attendu
+        function testNmb(){
+            if($gameSearch == $gameValue){
+                $gameResult.text('Well done dude! '+ $gameTry +' Appuie sur le bouton "Recommencer" pour rejouer!')
+                $('#gameReset').show(); // On affiche le btn reset
+            }else{
+                if($gameSearch > $gameValue){
+                    $gameResult.text('Pouet, c\'est moins !');// Si le nombre entré est supérieur au nombre attendu
+                }else{
+                    $gameResult.text('Pouet, c\'est plus !');// Si le nombre entré est inférieur au nombre attendu
+                }
+            }
+        }
+    });
+});
